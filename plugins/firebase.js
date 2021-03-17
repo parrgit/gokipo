de817d6b73f81da08289084713cc75551421690f
parrgit <kneeepain@gmail.com>
2021-03-17 23:58:49 +0900

    delete socket.vue

--- plugins/firebase.js
+++ plugins/firebase.js
@@ -17,6 +17,6 @@ export default ({ app }, inject) => {
   inject('fireFunc', firebase.functions())
 }
 
-// TODO最終的に治す
-const functions = firebase.functions()
-functions.useFunctionsEmulator('http://localhost:5000')
+// TODO最終的に消す
+// const functions = firebase.functions()
+// functions.useFunctionsEmulator('http://localhost:5000')
