22e7e58467701b7bc0666c31b26e58a6c1883bde
parrgit <kneeepain@gmail.com>
2021-04-08 00:06:03 +0900

    Fix プレゼンスの構築、uid取得できるように

--- plugins/realtime.js
+++ plugins/realtime.js
@@ -1,50 +1,56 @@
-// export default ({ app }) => {
-//   console.log(app.$fireAuth) //TODO消す
-//   // Fetch the current user's ID from Firebase Authentication.
-//   var uid = app.$fireAuth.currentUser.uid
+export default async ({ app }) => {
+  // Fetch the current user's ID from Firebase Authentication.
+  let uid
+  app.$fireAuth.onAuthStateChanged(function(user) {
+    if (user) {
+      uid = user.uid
+    } else {
+      return
+    }
+  })
 
-//   // Create a reference to this user's specific status node.
-//   // This is where we will store data about being online/offline.
-//   var userStatusDatabaseRef = app.$fireReal.ref('/status/' + uid)
+  // Create a reference to this user's specific status node.
+  // This is where we will store data about being online/offline.
+  var userStatusDatabaseRef = app.$fireReal.ref('/status/' + uid)
 
-//   // We'll create two constants which we will write to
-//   // the Realtime database when this device is offline
-//   // or online.
-//   var isOfflineForDatabase = {
-//     state: 'offline',
-//     last_changed: app.$firebase.database.ServerValue.TIMESTAMP,
-//   }
+  // We'll create two constants which we will write to
+  // the Realtime database when this device is offline
+  // or online.
+  var isOfflineForDatabase = {
+    state: 'offline',
+    last_changed: app.$firebase.database.ServerValue.TIMESTAMP,
+  }
 
-//   var isOnlineForDatabase = {
-//     state: 'online',
-//     last_changed: app.$firebase.database.ServerValue.TIMESTAMP,
-//   }
+  var isOnlineForDatabase = {
+    state: 'online',
+    last_changed: app.$firebase.database.ServerValue.TIMESTAMP,
+  }
 
-//   // Create a reference to the special '.info/connected' path in
-//   // Realtime Database. This path returns `true` when connected
-//   // and `false` when disconnected.
-//   app.$fireReal.ref('.info/connected').on('value', function(snapshot) {
-//     // If we're not currently connected, don't do anything.
-//     if (snapshot.val() == false) {
-//       return
-//     }
+  // Create a reference to the special '.info/connected' path in
+  // Realtime Database. This path returns `true` when connected
+  // and `false` when disconnected.
+  app.$fireReal.ref('.info/connected').on('value', function(snapshot) {
+    // If we're not currently connected, don't do anything.
+    if (snapshot.val() == false) {
+      return
+    }
 
-//     // If we are currently connected, then use the 'onDisconnect()'
-//     // method to add a set which will only trigger once this
-//     // client has disconnected by closing the app,
-//     // losing internet, or any other means.
-//     userStatusDatabaseRef
-//       .onDisconnect()
-//       .set(isOfflineForDatabase)
-//       .then(function() {
-//         // The promise returned from .onDisconnect().set() will
-//         // resolve as soon as the server acknowledges the onDisconnect()
-//         // request, NOT once we've actually disconnected:
-//         // https://firebase.google.com/docs/reference/js/firebase.database.OnDisconnect
+    // If we are currently connected, then use the 'onDisconnect()'
+    // method to add a set which will only trigger once this
+    // client has disconnected by closing the app,
+    // losing internet, or any other means.
+    userStatusDatabaseRef
+      .onDisconnect()
+      .set(isOfflineForDatabase)
+      .then(function() {
+        // The promise returned from .onDisconnect().set() will
+        // resolve as soon as the server acknowledges the onDisconnect()
+        // request, NOT once we've actually disconnected:
+        // https://firebase.google.com/docs/reference/js/firebase.database.OnDisconnect
 
-//         // We can now safely set ourselves as 'online' knowing that the
-//         // server will mark us as offline once we lose connection.
-//         userStatusDatabaseRef.set(isOnlineForDatabase)
-//       })
-//   })
-// }
+        // We can now safely set ourselves as 'online' knowing that the
+        // server will mark us as offline once we lose connection.
+        userStatusDatabaseRef.set(isOnlineForDatabase)
+      })
+  })
+}
